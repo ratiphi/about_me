d619ec55af8f3e9a85fc330e7b664b5bd2d52bbf
Tim Turner <timmyturner@me.com>
2017-05-09 13:56:17 -0700

    initial file creation

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var kids = prompt('how many kids does tim have?');
+console.log('number of kids:', kids);
+
+var hats = prompt('how many hats does tim have?');
+console.log('number of hats:', hats);
+
+if (kids === '3' || kids === '4') {
+  //do stuff here
+}
+
+if (kids === '3' && hats === '2') {
+  //do more stuff here
+}
